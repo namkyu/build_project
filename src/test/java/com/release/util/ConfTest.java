b0acdcbfc01618cd5cbe78de9d9fef6b59a09bf4
namkyu <lng1982@gmail.com>
2013-03-25 13:34:51 +0900

    ClassLoader 테스트

--- src/test/java/com/release/util/ConfTest.java
+++ src/test/java/com/release/util/ConfTest.java
@@ -1,6 +1,11 @@
 package com.release.util;
 
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
 import org.junit.Test;
 
@@ -14,6 +19,8 @@ import org.junit.Test;
  */
 public class ConfTest {
 
+	private final String FILE_NAME = "test.txt";
+
 	@Test
 	public void confInitTest() throws IOException {
 		Conf.init();
@@ -24,5 +31,35 @@ public class ConfTest {
 		System.out.println(Conf.getValue("ftp.remote.password"));
 		System.out.println(Conf.getValue("ftp.local.upload.directory"));
 		System.out.println(Conf.getValue("ftp.remote.upload.directory"));
+	}
+
+	@Test
+	public void resourceTest() {
+		URL url = getClass().getResource(FILE_NAME);
+		String path = url.getPath();
+		String fileName = path.substring(path.lastIndexOf("/") + 1);
+		assertThat(fileName, is(FILE_NAME));
+	}
+
+	@Test
+	public void resourceTest1() {
+		URL url = getClass().getClassLoader().getResource("com/release/util/" + FILE_NAME);
+		String path = url.getPath();
+		String fileName = path.substring(path.lastIndexOf("/") + 1);
+		assertThat(fileName, is(FILE_NAME));
+	}
+
+	@Test
+	public void resourceTest2() throws IOException {
+		InputStream is = getClass().getClassLoader().getResourceAsStream("com/release/util/" + FILE_NAME);
+		byte[] bts = new byte[4];
+		is.read(bts);
+		assertThat("test", is(new String(bts)));
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void resourceTest3() {
+		URL url = getClass().getClassLoader().getResource("/com/release/util/" + FILE_NAME);
+		url.getPath();
 	}
 }
